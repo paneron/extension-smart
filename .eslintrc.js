343e0dd898210aff3ed4b0c0569d81d28f92eda8
Jeffrey Lau <jeffrey.lau@ribose.com>
2022-05-25 11:18:09 +0800

    lint: Replace gts and prettier with just eslint
    There were problems adding more eslint rules wth prettier installed.
    Rules were conflicting, even with `eslint-{config,plugin}-prettier`
    configured.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,154 +1,149 @@
-module.exports = {
-  "env": {
-    "jest/globals": true
-  },
-  "extends": "./node_modules/gts/",
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": "./tsconfig.json"
-  },
-  "overrides": [
-    {
-      "files": [
-        "spec/**"
-      ],
-      "plugins": [
-        "jest"
-      ],
-      "extends": [
-        "plugin:jest/recommended",
-        "plugin:jest/style"
-      ],
-      "rules": {
-        "jest/prefer-expect-assertions": "off"
-      }
-    }
+var rules = {
+  'quotes' : [
+    'warn',
+    'single',
+    { avoidEscape: true },
   ],
-  "rules": {
-    "curly": "error",
-    "quotes": "off", // handled by prettier
-    "require-jsdoc": [
-      "off"
-    ],
-    "valid-jsdoc": [
-      "off"
-    ],
-    "indent": [
-      "off", // handled by prettier
-      2,
-      {
-        "SwitchCase": 1,
-        "VariableDeclarator": {
-          "var": 2,
-          "let": 2,
-          "const": 3
-        },
-        "ignoredNodes": [
-          "ConditionalExpression"
-        ]
-      }
-    ],
-    "key-spacing": [
-      "error",
-      {
-        "singleLine": {
-          "beforeColon": false,
-          "afterColon": true
-        },
-        "multiLine": {
-          "beforeColon": true,
-          "afterColon": true,
-          "align": "colon"
-        }
-      }
-    ],
-    "keyword-spacing": [
-      "error",
-      {
-        "before": true,
-        "after": true
-      }
-    ],
-    "spaced-comment": [
-      "error",
-      "always",
-      {
-        "exceptions": [
-          "-",
-          "+",
-          "=",
-          "*"
-        ],
-        "markers": [
-          "=",
-          "*/",
-          "/*",
-          "X",
-          "//"
-        ]
-      }
-    ],
-    "no-multi-spaces": [
-      1,
-      {
-        "exceptions": {
-          "VariableDeclarator": true
-        }
-      }
-    ],
-    "no-cond-assign": [
-      2,
-      "except-parens"
-    ],
-    "no-redeclare": [
-      "error",
-      {
-        "builtinGlobals": true
-      }
-    ],
-    "dot-notation": [
-      2,
-      {
-        "allowKeywords": true
-      }
-    ],
-    "eqeqeq": [
-      2,
-      "smart"
-    ],
-    "no-plusplus": [
-      "warn",
-      {
-        "allowForLoopAfterthoughts": true
-      }
-    ],
-    "one-var": [
-      "off", // Enable once tests are set up
-      "consecutive"
-    ],
-    "object-curly-spacing": [
-      "error",
-      "always",
-      {
-        "objectsInObjects": false,
-        "arraysInObjects": false
-      }
-    ],
-    "quote-props": [
-      "error",
-      "consistent-as-needed",
-      {
-        "keywords": true
-      }
-    ],
-    "camelcase": [
-      "warn"
-    ],
-    "max-len": [
-      "warn"
-    ],
-    "new-cap": [
-      "warn"
-    ]
-  }
-}
+  "require-jsdoc" : ["off"],
+  "valid-jsdoc"   : ["off"],
+  "indent"        : [
+    "error",
+    2,
+    {
+      SwitchCase         : 1,
+      VariableDeclarator : {
+        "var"   : 2,
+        "let"   : 2,
+        "const" : 3,
+      },
+      ignoredNodes : ["ConditionalExpression"],
+    },
+  ],
+  "key-spacing" : [
+    "error",
+    {
+      singleLine : {
+        beforeColon : true,
+        afterColon  : true,
+      },
+      multiLine : {
+        beforeColon : true,
+        afterColon  : true,
+        align       : "colon",
+      },
+    },
+  ],
+  "keyword-spacing" : [
+    "error",
+    {
+      before : true,
+      after  : true,
+    },
+  ],
+  "spaced-comment" : [
+    "error",
+    "always",
+    {
+      exceptions : ["-", "+", "=", "*"],
+      markers    : ["=", "*/", "/*", "X", "//"],
+    },
+  ],
+  "no-multi-spaces" : [
+    1,
+    {
+      exceptions : {
+        VariableDeclarator : true,
+      },
+    },
+  ],
+  "no-cond-assign" : [2, "except-parens"],
+  "no-redeclare"   : [
+    "error",
+    {
+      builtinGlobals : true,
+    },
+  ],
+  "dot-notation" : [
+    2,
+    {
+      allowKeywords : true,
+    },
+  ],
+  "eqeqeq"      : [2, "smart"],
+  "no-plusplus" : [
+    "warn",
+    {
+      allowForLoopAfterthoughts : true,
+    },
+  ],
+  "one-var" : [
+    "off", // Enable once tests are set up
+    "consecutive",
+  ],
+  "object-curly-spacing" : [
+    "error",
+    "always",
+    {
+      objectsInObjects : false,
+      arraysInObjects  : false,
+    },
+  ],
+  "quote-props" : [
+    "error",
+    "consistent-as-needed",
+    {
+      keywords : true,
+    },
+  ],
+  "camelcase" : ["warn"],
+  "max-len"   : ["warn"],
+  "new-cap"   : ["warn"],
+
+  "key-spacing" : [
+    "error",
+    {
+      singleLine : {
+        beforeColon : true,
+        afterColon  : true,
+      },
+      multiLine : {
+        beforeColon : true,
+        afterColon  : true,
+        align       : "colon",
+      },
+    },
+  ],
+};
+
+module.exports = {
+  root      : true,
+  "env"   : {
+    // "jest/globals" : true,
+  },
+  "extends" : [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    // "plugin:prettier/recommended",
+    // "./node_modules/gts/"
+  ],
+  "parser"        : "@typescript-eslint/parser",
+  "parserOptions" : {
+    project : "./tsconfig.json",
+  },
+  "rules" : rules,
+  overrides : [
+    {
+      files : ["*.js"],
+      rules : rules,
+    },
+    {
+      "files"   : ["spec/**"],
+      "plugins" : ["jest"],
+      "extends" : ["plugin:jest/recommended", "plugin:jest/style"],
+      "rules"   : {
+        "jest/prefer-expect-assertions" : "off",
+      },
+    },
+  ],
+  rules : rules,
+};
