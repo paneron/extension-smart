a8bd55e911dfb6fb349f2668089b51a56f039d87
Jeffrey Lau <jeffrey.lau@ribose.com>
2022-08-17 12:20:18 +0800

    babel: Add settings according to jest guide

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+module.exports = api => {
+  const isTest = api.env('test'); // TODO: use isTest boolean where needed
+  return {
+    presets: [
+      ['@babel/preset-env', { targets: { node: 'current' } }],
+      '@babel/preset-typescript',
+    ]
+  };
 };
