267948f7891a11b41af0e7a5273524dfa961972d
Jeffrey Lau <jeffrey.lau@ribose.com>
2022-08-31 15:32:14 +0800

    ci: Make jest run under nodejs 14.x

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,12 @@
 module.exports = api => {
   const isTest = api.env('test'); // TODO: use isTest boolean where needed
   return {
-    presets: [
-      ['@babel/preset-env', { targets: { node: 'current' } }],
+    // logical assignment operators, e.g. `||=` , for nodejs 14.x
+    plugins : ['@babel/plugin-proposal-logical-assignment-operators'],
+    presets : [
+      ['@babel/preset-env', { targets : { electron : '10' }}],
       '@babel/preset-typescript',
+      '@babel/preset-react',
     ]
   };
 };
