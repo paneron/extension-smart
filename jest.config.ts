f1f921e9cddd0db1fd864dd86e09572c6577a0e1
Jeffrey Lau <jeffrey.lau@ribose.com>
2022-08-31 15:33:07 +0800

    Format some source

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,6 @@
 /*
- * For a detailed explanation regarding each configuration property and type check, visit:
+ * For a detailed explanation regarding each configuration property and type
+ * check, visit:
  * https://jestjs.io/docs/configuration
  */
 
@@ -13,17 +14,20 @@ export default {
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "/private/var/folders/v7/bpr811_x73bfcjbzg8mvb_v40000gq/T/jest_dz",
 
-  // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: true,
+  // Automatically clear mock calls, instances, contexts and results before
+  // every test
+  clearMocks : true,
 
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  // Indicates whether the coverage information should be collected while
+  // executing the test
+  collectCoverage : true,
 
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  // An array of glob patterns indicating a set of files for which coverage
+  // information should be collected
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory : 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -41,7 +45,8 @@ export default {
   //   "clover"
   // ],
 
-  // An object that configures minimum threshold enforcement for coverage results
+  // An object that configures minimum threshold enforcement for coverage
+  // results
   // coverageThreshold: undefined,
 
   // A path to a custom dependency extractor
@@ -55,22 +60,29 @@ export default {
   //   "enableGlobally": false
   // },
 
-  // Force coverage collection from ignored files using an array of glob patterns
+  // Force coverage collection from ignored files using an array of glob
+  // patterns
   // forceCoverageMatch: [],
 
-  // A path to a module which exports an async function that is triggered once before all test suites
+  // A path to a module which exports an async function that is triggered once
+  // before all test suites
   // globalSetup: undefined,
 
-  // A path to a module which exports an async function that is triggered once after all test suites
+  // A path to a module which exports an async function that is triggered once
+  // after all test suites
   // globalTeardown: undefined,
 
-  // A set of global variables that need to be available in all test environments
+  // A set of global variables that need to be available in all test
+  // environments
   // globals: {},
 
-  // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
+  // The maximum amount of workers used to run your tests. Can be specified as
+  // % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as
+  // the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
 
-  // An array of directory names to be searched recursively up from the requiring module's location
+  // An array of directory names to be searched recursively up from the
+  // requiring module's location
   // moduleDirectories: [
   //   "node_modules"
   // ],
@@ -87,10 +99,12 @@ export default {
   //   "node"
   // ],
 
-  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  // A map from regular expressions to module names or to arrays of module
+  // names that allow to stub out resources with a single module
   // moduleNameMapper: {},
 
-  // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
+  // An array of regexp pattern strings, matched against all module paths
+  // before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
 
   // Activates notifications for test results
@@ -132,16 +146,20 @@ export default {
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
 
-  // The paths to modules that run some code to configure or set up the testing environment before each test
+  // The paths to modules that run some code to configure or set up the testing
+  // environment before each test
   // setupFiles: [],
 
-  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  // A list of paths to modules that run some code to configure or set up the
+  // testing framework before each test
   // setupFilesAfterEnv: [],
 
-  // The number of seconds after which a test is considered as slow and reported as such in the results.
+  // The number of seconds after which a test is considered as slow and
+  // reported as such in the results.
   // slowTestThreshold: 5,
 
-  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
+  // A list of paths to snapshot serializer modules Jest should use for
+  // snapshot testing
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
@@ -159,7 +177,8 @@ export default {
   //   "**/?(*.)+(spec|test).[tj]s?(x)"
   // ],
 
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  // An array of regexp pattern strings that are matched against all test paths,
+  // matched tests are skipped
   // testPathIgnorePatterns: [
   //   "/node_modules/"
   // ],
@@ -175,20 +194,23 @@ export default {
 
   // A map from regular expressions to paths to transformers
   // transform: undefined,
-
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  //
+  // An array of regexp pattern strings that are matched against all source
+  // file paths, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "/node_modules/",
   //   "\\.pnp\\.[^\\/]+$"
   // ],
 
-  // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
+  // An array of regexp pattern strings that are matched against all modules
+  // before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
 
   // Indicates whether each individual test should be reported during the run
   // verbose: undefined,
 
-  // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
+  // An array of regexp patterns that are matched against all source file paths
+  // before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
 
   // Whether to use watchman for file crawling
