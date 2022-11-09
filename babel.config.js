4f7201f8a73e6aa8af2022c7a05bac742df3f702
Jeffrey Lau <jeffrey.lau@ribose.com>
2022-11-09 17:26:17 +0800

    chore: Make build script run babel only
    for dev performance reasons.

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,24 @@
 module.exports = api => {
   const isTest = api.env('test'); // TODO: use isTest boolean where needed
   return {
+    exclude : [
+      '*.test.*',
+      '**/*.test.*'
+    ],
+    ignore : [
+      '*.test.*',
+      '**/*.test.*'
+    ],
+    compact : true,
     // logical assignment operators, e.g. `||=` , for nodejs 14.x
-    plugins : ['@babel/plugin-proposal-logical-assignment-operators'],
+    plugins : [
+      '@babel/plugin-proposal-logical-assignment-operators',
+      '@babel/plugin-transform-typescript',
+    ],
     presets : [
       ['@babel/preset-env', { targets : { electron : '10' }}],
-      '@babel/preset-typescript',
-      '@babel/preset-react',
+      ['@babel/preset-typescript', { isTSX : true, allExtensions : true }],
+      ['@babel/preset-react', { flow : false, typescript : true }],
     ]
   };
 };
